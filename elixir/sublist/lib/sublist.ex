e0233f75511b17b9892ff17ede2c2c6aa68d1c79
David Ferrari <davidchristenes13@gmail.com>
2024-09-09 00:41:27 -0300

    elixir

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -3,15 +3,21 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, a) do
-    :equal
-  end
 
   def compare(a, b) do
-    case List.myers_difference(a, b) do
-      [{:eq, elements}] when elements == b -> :eq
-      [{:ins, elements}] -> :sublist
-      [{:del, elements}] -> :del
+    cond do
+      equal?(a, b) -> :equal
+      sublist?(b, a) -> :sublist
+      sublist?(a, b) -> :superlist
+      true -> :unequal
     end
+  end
+
+  defp equal?(a, b), do: a === b
+
+  defp sublist?(a, b) when length(a) < length(b), do: false
+  defp sublist?(a, b) do
+    sub = Enum.take(a, length(b))
+    equal?(sub, b) or sublist?(tl(a), b)
   end
 end
